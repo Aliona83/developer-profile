6f4e03404e7e4e167dee038af4bbe62be1b84b49
Aliona <alionajanusko@gmail.com>
2024-01-31 13:22:30 +0000

    Create section for main page, add image

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 const express = require('express');
-const path = require('path'); // Import the path module
+const path = require('path');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the 'public' folder
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    const indexPath = path.join(__dirname, 'index.html');
-    res.sendFile(indexPath);
-});
-
-app.get('/about', (req, res) => {
-    res.send('This is the about page.');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
